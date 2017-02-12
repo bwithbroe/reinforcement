ffadc983857fa39dec68984aff9afeca144a8d0b
dylandforbes <dylandforbes@gmail.com>
2017-02-12 14:36:29 -0600

    a bit

--- qlearningAgents.py
+++ qlearningAgents.py
@@ -39,10 +39,8 @@ class QLearningAgent(ReinforcementAgent):
           which returns legal actions for a state
     """
     def __init__(self, **args):
-        "You can initialize Q-values here..."
         ReinforcementAgent.__init__(self, **args)
-
-        "*** YOUR CODE HERE ***"
+        self.qValues = util.Counter()
 
     def getQValue(self, state, action):
         """
@@ -50,8 +48,7 @@ class QLearningAgent(ReinforcementAgent):
           Should return 0.0 if we have never seen a state
           or the Q node value otherwise
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return float(self.qValues[(state, action)])
 
 
     def computeValueFromQValues(self, state):
@@ -61,8 +58,9 @@ class QLearningAgent(ReinforcementAgent):
           there are no legal actions, which is the case at the
           terminal state, you should return a value of 0.0.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        if not self.getLegalActions(state):
+            return 0.0
+        return max(self.getLegalActions(state))
 
     def computeActionFromQValues(self, state):
         """
@@ -70,8 +68,9 @@ class QLearningAgent(ReinforcementAgent):
           are no legal actions, which is the case at the terminal state,
           you should return None.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        if self.isTerminal(state):
+            return None
+        return max(self.getLegalActions(state), key=lambda action: self.getQValue(state, action))
 
     def getAction(self, state):
         """
@@ -101,8 +100,7 @@ class QLearningAgent(ReinforcementAgent):
           NOTE: You should never call this function,
           it will be called on your behalf
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+
 
     def getPolicy(self, state):
         return self.computeActionFromQValues(state)
